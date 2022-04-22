c4417609b7e88f00f624bbc21be41dc7baea8f5a
Andreas3333 <75769702+Andreas3333@users.noreply.github.com>
2022-04-22 14:21:36 -0400

    php post methods added file: insert.php

--- PHP/insertDB.php
+++ PHP/insertDB.php
@@ -1,2 +1,45 @@
-$con = mysqli_connect("localhost","root@localhost","your_localhost_database_password","AnimalPicker");
+<?php
+
+// Grab User submitted information
+$ = $_POST["user"];
+$ = $_POST["pass"];
+
+// Create connection #servername, username, password, db 
+$conn = mysqli_connect("localhost", "root@localhost", "","AnimalPicker");
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+
+echo"Conected To Data Base";
+
+$conn->autocommit(false);
+$error= array();
+
+$a=$conn-> query("Insert into	account_info values(1099,'Sam',3450)");
+
+if(a==false){
+	array_push($error,'Problem');
+}
+
+$b=$conn-> query("Insert into	account_info values(20199,'Sabil',5550)");
+
+if(b==false){
+	array_push($error,'Problem');
+}
+
+//array_push($error,'Problem');
+
+if(!empty($error)){
+	
+	$conn->rollback();
+	echo "Transaction Unsuccessful!";
+
+}
+else{
+	echo "Transaction Successful!";
+	$conn->commit();
+}
+?>
+
 
