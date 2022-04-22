100818b9f9395bca11dbbf934eff0da8f2c0550e
Andreas3333 <75769702+Andreas3333@users.noreply.github.com>
2022-04-22 15:01:04 -0400

    attributes added to insert.php/ --sql insert statment needs completed--

--- PHP/insertDB.php
+++ PHP/insertDB.php
@@ -1,8 +1,19 @@
 <?php
 
 // Grab User submitted information
-$animaltype = $_POST["animaltype"];
 $age = $_POST["age"];
+$breed = $_POST["breed"];
+$color = $_POST["color"];
+$declawed = $_POST["declawed"];
+$location_name = $_POST["location_name"];
+$personality_statement = $_POST["personality_statement"];
+$pet_id = $_POST["pet_id"];
+$price_id = $_POST["price_id"];
+$sex = $_POST["sex"];
+$sprayed_or_neutered = $_POST["sprayed_or_neutered"];
+$speices = $_POST["speices"];
+$vaccination = $_POST["vaccination"];
+$weight = $_POST["weight"];
 
 // Create connection #servername, username, password, db 
 $conn = mysqli_connect("localhost", "root@localhost", "","AnimalPicker");
@@ -12,7 +23,7 @@ if (!$conn) {
 }
 
 // database insert SQL code
-$sql = "INSERT INTO `animals` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+$sql = "INSERT INTO `animals` (`age`, `breed`, `color`, `declawed`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
 
 
 ?>
