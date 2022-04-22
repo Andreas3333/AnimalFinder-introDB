cc4a91691f9f51cfbdec9253ced28794d8b979e3
Andreas3333 <75769702+Andreas3333@users.noreply.github.com>
2022-04-22 14:25:21 -0400

    more revision

--- PHP/insertDB.php
+++ PHP/insertDB.php
@@ -11,35 +11,10 @@ if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
 }
 
-echo"Conected To Data Base";
+// database insert SQL code
+$sql = "INSERT INTO `` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
 
-$conn->autocommit(false);
-$error= array();
 
-$a=$conn-> query("Insert into	account_info values(1099,'Sam',3450)");
-
-if(a==false){
-	array_push($error,'Problem');
-}
-
-$b=$conn-> query("Insert into	account_info values(20199,'Sabil',5550)");
-
-if(b==false){
-	array_push($error,'Problem');
-}
-
-//array_push($error,'Problem');
-
-if(!empty($error)){
-	
-	$conn->rollback();
-	echo "Transaction Unsuccessful!";
-
-}
-else{
-	echo "Transaction Successful!";
-	$conn->commit();
-}
 ?>
 
 
